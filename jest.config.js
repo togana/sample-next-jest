9726a152341ff6d977b7916816beaff5a2f00abf
togana <o.togana@gmail.com>
2021-11-18 20:45:47 +0900

    next/jest を利用してみた
    デフォルト値が下記のようになるので customJestConfig で追加した
    {
      moduleNameMapper: {
        '^.+\\.module\\.(css|sass|scss)$': '{ローカル環境}/node_modules/next/dist/build/jest/object-proxy.js',
        '^.+\\.(css|sass|scss)$': '{ローカル環境}/node_modules/next/dist/build/jest/__mocks__/styleMock.js',
        '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '{ローカル環境}/node_modules/next/dist/build/jest/__mocks__/fileMock.js'
      },
      testPathIgnorePatterns: [ '/node_modules/', '/.next/' ],
      transform: {
        '^.+\\.(js|jsx|ts|tsx)$': [
          '{ローカル環境}/node_modules/next/dist/build/swc/jest-transformer.js',
         {
            nextConfig: undefined,
            jsConfig: undefined,
            resolvedBaseUrl: undefined,
            isEsmProject: false
          }
        ]
      },
      transformIgnorePatterns: [ '/node_modules/', '^.+\\.module\\.(css|sass|scss)$' ]
    }

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
-module.exports = {
+const nextJest = require('next/jest');
+// Optionally provide path to Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest();
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
   moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
-    // https://jestjs.io/docs/webpack#mocking-css-modules
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
     // Handle CSS imports (without CSS modules)
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
 
@@ -19,15 +21,7 @@ module.exports = {
     // Handle module aliases
     '^@/components/(.*)$': '<rootDir>/components/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
-}
+};
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
